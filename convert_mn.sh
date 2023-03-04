4fc31d991210cc1c353c42c480dc3b6d9bf04e89
Kevin Eales <kev0.0.0.255@gmail.com>
2023-03-04 12:32:55 -0500

    Reworked variables

--- convert_mn.sh
+++ convert_mn.sh
@@ -16,27 +16,27 @@ if [ -z "${downsample}"    ]; then echo "Error: ARG DOWNSAMPLE    not specified.
     && if [ -z "${precision}" ]; then echo "Error: ARG PRECISION not specified."; exit 1; fi \
     && if [ -z "${workspace}" ]; then echo "Error: ARG WORKSPACE not specified."; exit 1; fi
 
-target="mnv3-trt$trt_version-fp$precision-$rez-ds$downsample.engine"
+target="mnv3-trt${trt_version}-fp${precision}-${rez}-ds${downsample}.engine"
 
 echo
-echo building engine: $target
+echo building engine: ${target}
 echo
 sleep 5
-wget https://github.com/PeterL1n/RobustVideoMatting/releases/download/v1.0.0/rvm_mobilenetv3_fp$precision.onnx
+wget https://github.com/PeterL1n/RobustVideoMatting/releases/download/v1.0.0/rvm_mobilenetv3_fp${precision}.onnx
 
-shapes=$(python rvm_onnx_infer.py --model "rvm_mobilenetv3_fp$precision.onnx" --input-image "input.jpg" --precision float$precision --downsample-ratio $downsample --input-resize $width $height)
+shapes=$(python rvm_onnx_infer.py --model "rvm_mobilenetv3_fp${precision}.onnx" --input-image "input.jpg" --precision float${precision} --downsample-ratio ${downsample} --input-resize ${width} ${height})
 
-echo $shapes
+echo ${shapes}
 
-python -m onnxsim rvm_mobilenetv3_fp${precision}.onnx rvm_mobilenetv3_fp${precision}_sim.onnx --overwrite-input-shape $shapes
+python -m onnxsim rvm_mobilenetv3_fp${precision}.onnx rvm_mobilenetv3_fp${precision}_sim.onnx --overwrite-input-shape ${shapes}
 
-python rvm_onnx_modify.py -i rvm_mobilenetv3_fp${precision}_sim.onnx --downsample-ratio $downsample
+python rvm_onnx_modify.py -i rvm_mobilenetv3_fp${precision}_sim.onnx --downsample-ratio ${downsample}
 
 # https://github.com/aadhithya/onnx-typecast.git  Convert all int64 values to int 32
 python convert.py rvm_mobilenetv3_fp${precision}_sim_modified.onnx rvm_mobilenetv3_fp${precision}_sim_modified_fp${precision}.onnx 32
 
-trt_shapes=$(python translate_inputs.py --onnx-shapes "$shapes" --size $width $height)
+trt_shapes=$(python translate_inputs.py --onnx-shapes "${shapes}" --size ${width} ${height})
 
-echo $trt_shapes
+echo ${trt_shapes}
 
-trtexec --onnx=rvm_mobilenetv3_fp${precision}_sim_modified_fp${precision}.onnx --streams=2 --exposeDMA --workspace=$workspace --saveEngine=$target --verbose # --useDLACore=0 --allowGPUFallback # --optShapes=$trt_shapes
+trtexec --onnx=rvm_mobilenetv3_fp${precision}_sim_modified_fp${precision}.onnx --streams=2 --exposeDMA --workspace=${workspace} --saveEngine=${target} --verbose # --useDLACore=0 --allowGPUFallback # --optShapes=$trt_shapes
