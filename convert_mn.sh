0570ef9188261c20dfe48c0096263d384dc24d87
Kevin Eales <kev0.0.0.255@gmail.com>
2023-03-04 12:01:14 -0500

    added trt version detection

--- convert_mn.sh
+++ convert_mn.sh
@@ -7,6 +7,7 @@ width=$3
 height=$4
 precision=$5
 workspace=$6
+trt_version=$(tr -d '.-' <<< $(echo $(echo $(echo $(dpkg -l | grep nvinfer-plugin-dev) | cut -d " " -f 3)) | cut -d "+" -f 1))
 
 if [ -z "${downsample}"    ]; then echo "Error: ARG DOWNSAMPLE    not specified."; exit 1; fi \
     && if [ -z "${rez}"   ]; then echo "Error: ARG REZ   not specified."; exit 1; fi \
@@ -15,7 +16,7 @@ if [ -z "${downsample}"    ]; then echo "Error: ARG DOWNSAMPLE    not specified.
     && if [ -z "${precision}" ]; then echo "Error: ARG PRECISION not specified."; exit 1; fi \
     && if [ -z "${workspace}" ]; then echo "Error: ARG WORKSPACE not specified."; exit 1; fi
 
-target="mnv3-trt8517-fp$precision-$rez-ds$downsample.engine"
+target="mnv3-trt$trt_version-fp$precision-$rez-ds$downsample.engine"
 
 echo
 echo target $target
